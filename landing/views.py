cc2e9f449bf8cb92aa1168f48a1364e68a36379f
Fredo <fredotanzil@gmail.com>
2025-05-15 11:24:05 +0700

    progress on landing page

--- landing/views.py
+++ landing/views.py
@@ -8,12 +8,14 @@ from django_user_agents.utils import get_user_agent
 def home(request):
     user_agent = get_user_agent(request)
     
-    if user_agent.is_mobile:
-        template = "mobile/index.html"
-    elif user_agent.is_tablet:
-        template = "desktop/index.html"
-    else:
-        template = "desktop/index.html"
+    # if user_agent.is_mobile:
+    #     template = "mobile/index.html"
+    # elif user_agent.is_tablet:
+    #     template = "desktop/index.html"
+    # else:
+    #     template = "desktop/index.html"
+
+    template = "BetterLandingPage.html"
 
     return render(request, template)
 
